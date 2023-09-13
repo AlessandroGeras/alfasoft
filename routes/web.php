9043864bee4de86f92bfadd6eb84e936ddf1cd04
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2023-09-13 13:34:54 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +9,20 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [ContactController::class, "index"])->name("index.contact");
+
+Route::get("/contact/register", [ContactController::class, "register"])->name("register.contact.view");
+Route::post("/contact/register", [ContactController::class, "store"])->name("register.contact");
+
+Route::get("/contact/{id}", [ContactController::class, "show"])->name("show.contact");
+
+Route::get("/edit/{id}", [ContactController::class, "edit"])->name("edit.contact");
+Route::post("/edit/{id}", [ContactController::class, "update"])->name("update.contact");
+
+Route::get("/delete/{contact}", [ContactController::class, "softDelete"])->name("delete.contact");
+Route::delete("/destroy/{contact}", [ContactController::class, "destroy"])->name("destroy.contact");
