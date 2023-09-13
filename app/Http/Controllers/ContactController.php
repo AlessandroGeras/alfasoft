9e282ace1c79d5b8d705aef3115426a952ce9c8f
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2023-09-13 14:53:40 -0300

    Controller, Requests & Model

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ContactStoreRequest;
+use App\Http\Requests\ContactUpdateRequest;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        
+
+        return view("index");
+    }
+
+    public function show($id)
+    {
+        return view('show_contact', [
+            'contact' => Contact::query()->findOrFail($id),
+        ]);
+    }
+
+    public function register()
+    {
+        return view("register_contact");
+    }
+
+    public function store(ContactStoreRequest $request)
+    {
+        Contact::query()->create($request->all());
+
+        return redirect("/");
+    }
+
+    public function edit($id)
+    {
+        $contact = Contact::query()->find($id);
+        return view("edit_contact", compact("contact"));
+    }
+
+    public function update(ContactUpdateRequest $request, $id)
+    {
+        $movie = Contact::query()->find($id);
+        $movie->update($request->all());
+
+        return redirect("/");
+    }
+
+    public function softDelete($id)
+    {
+        $contact = Contact::query()->find($id);
+        $contact->delete();
+
+        return redirect("/");
+    }
+
+    public function destroy($id)
+    {
+        $contact = Contact::query()->find($id);
+        $contact->forceDelete();
+
+        return redirect("/");
+    }
+}
